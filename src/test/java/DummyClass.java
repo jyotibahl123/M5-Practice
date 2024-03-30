01fe7058e193a57a1b3ef687fae3a50d5548cd8f
kajol <kajol@DESKTOP-UFCUJR1>
2024-03-31 02:50:32 +0530

    Pushing DummyClass

---
+++ src/test/java/DummyClass.java
@@ -0,0 +1,15 @@
+
+public class DummyClass {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("abc");
+		
+		System.out.println("def");
+		
+		System.out.println("ghi");
+
+	}
+
+}
