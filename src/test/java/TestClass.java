9e70940e2c4369afe84597b40b1636b393d573f0
kajol <kajol@DESKTOP-UFCUJR1>
2024-04-03 22:20:44 +0530

    Demo

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,5 +1,7 @@
  import org.testng.annotations.*;
+ import org.openqa.selenium.chrome.ChromeDriver;
 
+ import org.openqa.selenium.*;
 
 public class TestClass {
 	
@@ -7,7 +9,17 @@ public class TestClass {
 	@Test
 	public void Test1()
 	{
-		System.out.println("TC 001");
+		
+		WebDriver  driver = new ChromeDriver();
+		
+		driver.get("http://localhost:8888/");
+		
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+        driver.findElement(By.id("submitButton")).click();
+			
+		System.out.println("Login Successful");
+		
 	}
 	
 
